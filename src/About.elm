174eb5b40b14e847ad09cba6286e088b488d89c0
jdoi <jdoi.work@gmail.com>
2019-02-17 16:57:26 +0900

    スッキリさせた

--- src/About.elm
+++ src/About.elm
@@ -1,9 +1,6 @@
 module About exposing (..)
 
-import Task    exposing (Task)
-import Result exposing (toMaybe)
 import Json.Decode as Json exposing (field, map2)
---import Http exposing (get)
 import Http 
 import Html exposing (..)
 import Html.Attributes exposing
@@ -20,10 +17,10 @@ type alias About =
   , url   : String
   }
 
-type Action = Request
-            | Response (Result Http.Error Model)
+type Msg = Request
+         | Response (Result Http.Error Model)
 
-view : List About -> Html Action
+view : List About -> Html Msg
 view items =
   main_ []
     [ h1 [ id "home-logo"] [text "jdoi.pw"]
@@ -31,7 +28,7 @@ view items =
          (List.map jumpTo items) 
     ]
 
-jumpTo : About -> Html Action
+jumpTo : About -> Html Msg
 jumpTo item =
   li []
     [ a [href item.url]
@@ -40,20 +37,13 @@ jumpTo item =
       ]
     ]
 
-getItems : Cmd Action
+getItems : Cmd Msg
 getItems =
   Http.get
     { url = "./about.json"
     , expect = Http.expectJson Response decoder
     }
   
---itemsDecoder : decoder 
-  -- get decoder "dst/about.json"
-  --   |> toMaybe
-  --   |> Task.map (Maybe.withDefault [])
-  --   |> Task.map Response
-  --   |> Cmd.batch
-
 decoder : Json.Decoder Model
 decoder =
   Json.at ["about"]
@@ -65,11 +55,11 @@ aboutDecoder =
     (field "title" Json.string)
     (field "url" Json.string)
 
-update : Action -> Model -> (Model, Cmd Action)
-update action model =
-  case action of
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
     Request        -> ([], getItems)
-    Response result -> -- (items, Cmd.none)
+    Response result ->
       case result of
         Ok items -> (items, Cmd.none)
         Err _ -> ([], Cmd.none)
--- src/Main.elm
+++ src/Main.elm
@@ -2,7 +2,6 @@ module Main exposing (..)
 
 -- Vendor Libraries
 import Html     exposing (..)
---import Task     exposing (Task)
 import Browser
 
 -- My Libraries
@@ -10,7 +9,7 @@ import About
 
 type alias Model = About.Model
 
-type alias Msg = About.Action
+type alias Msg = About.Msg
 
 main = Browser.element
     { init = init
@@ -19,48 +18,20 @@ main = Browser.element
     , view = view
     }
 
--- type alias Model =
---     {
---         name : String
---     }
-
-init : () -> (About.Model, Cmd About.Action)
+init : () -> (About.Model, Cmd About.Msg)
 init _ = appInit
 
 update = appUpdate
 view = appView
-subscriptions : About.Model -> Sub About.Action
+subscriptions : About.Model -> Sub About.Msg
 subscriptions model =
     Sub.none
--- type Msg
---     = Name String
 
--- update : Msg -> Model -> Model
--- update msg model =
---     case msg of
---         Name name ->
---             { model | name = name }
-
--- view : Model -> Html Msg
--- view model =
---     div [] [text "hello"]
--- app : App Model
--- app =
---     start { init   = appInit
---           , view   = appView
---           , update = appUpdate
---           , inputs = []
---           }
-{-
-port tasks : Signal (Task Never ())
-port tasks =
-    app.tasks
--}
-appInit : (About.Model, Cmd About.Action)
+appInit : (About.Model, Cmd About.Msg)
 appInit = ([], About.getItems)
 
-appView : About.Model -> Html About.Action
+appView : About.Model -> Html About.Msg
 appView = About.view
 
-appUpdate : Msg -> Model -> (Model, Cmd About.Action)
+appUpdate : Msg -> Model -> (Model, Cmd About.Msg)
 appUpdate = About.update
